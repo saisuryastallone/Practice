ae2aaa3dbb319ffd82ca7c8bee30d97303010941
sss <sss@sss-linux>
2020-08-06 13:02:24 +0100

    First Commit

---
+++ OOPS/src/Classes.java
@@ -0,0 +1,31 @@
+
+public class Classes {
+ private String a;
+ public int b;
+ protected boolean c;
+ 
+ public void setb(int b) {
+	 this.b=b;
+ }
+ public int getb() {
+	 return b;
+ }
+public String getA() {
+	return a;
+}
+public void setA(String a) {
+	this.a = a;
+}
+public int getB() {
+	return b;
+}
+public void setB(int b) {
+	this.b = b;
+}
+public boolean isC() {
+	return c;
+}
+public void setC(boolean c) {
+	this.c = c;
+}
+}
