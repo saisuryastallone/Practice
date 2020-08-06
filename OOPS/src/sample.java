d0152676aa128880eb4265f2353c2c45aa992ba6
sss <sss@sss-linux>
2020-08-06 13:17:17 +0100

    First Commit

---
+++ OOPS/src/sample.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class sample {
+	public static void main(String[] args) {
+		Classes ab = new Classes();
+		ab.setb(5);
+		System.out.println(ab.getb());
+		 
+    }
+
+     
+}
